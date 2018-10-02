d16b71035cc38b8d103c8fa3f9410359f5e58965
fbaconnais2018 <fbaconnais2018@27-UC37-24.ad.campus-eni.fr>
2018-10-02 08:59:20 +0200

    Fin DAOEpreuveJdbcImpl, on continue!

--- src/dal/jdbc/DAOEpreuveJdbcImpl.java
+++ src/dal/jdbc/DAOEpreuveJdbcImpl.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+import bo.Candidat;
 import bo.Epreuve;
 import bo.Test;
 import dal.ConnectionProvider;
@@ -16,11 +17,14 @@ import dal.DAOEpreuve;
 public class DAOEpreuveJdbcImpl implements DAOEpreuve {
 	private Connection conn = null;
 	String selectOne = "SELECT " + "e.idEpreuve," + "e.dateDedutValidite," + "e.dateFinValidite," + "e.etat,"
-			+ "e.note_obtenue," + "e.niveau_obtenu," + "t.idTest," + "t.libelle," + "t.description," + "t.duree," + "u.nom,"
-			+ "u.prenom," + "u.email," + "FROM Epreuve e join Test t on (e.idTest = t.idTest)" + "join Utilisateur u on (e.idUtilisateur = u.idUtilisateur)" + "where e.idEpreuve=?";
+			+ "e.note_obtenue," + "e.niveau_obtenu," + "t.idTest," + "t.libelle," + "t.description," + "t.duree,"
+			+ "u.idUtilisateur," + "u.nom," + "u.prenom," + "u.email,"
+			+ "FROM Epreuve e join Test t on (e.idTest = t.idTest)"
+			+ "join Utilisateur u on (e.idUtilisateur = u.idUtilisateur)" + "where e.idEpreuve=?";
 	String selectAll = "SELECT " + "e.idEpreuve," + "e.dateDedutValidite," + "e.dateFinValidite," + "e.etat,"
-			+ "e.note_obtenue," + "e.niveau_obtenu," + "t.idTest," + "t.libelle," + "t.description," + "t.duree," + "u.nom,"
-			+ "u.prenom," + "u.email," + "FROM Epreuve e join Test t on (e.idTest = t.idTest)" + "join Utilisateur u on (e.idUtilisateur = u.idUtilisateur)";
+			+ "e.note_obtenue," + "e.niveau_obtenu," + "t.idTest," + "t.libelle," + "t.description," + "t.duree,"
+			+ "u.nom," + "u.prenom," + "u.email," + "FROM Epreuve e join Test t on (e.idTest = t.idTest)"
+			+ "join Utilisateur u on (e.idUtilisateur = u.idUtilisateur)";
 
 	String remove = "DELETE FROM Epreuve WHERE idEpreuve = ?";
 	String add = "INSERT INTO Epreuve (dateDedutValidite, dateFinValidite, tempsEcoule, etat, note_obtenue, niveau_obtenu, idTest, idUtilisateur) VALUES (?, ?, ?, ?, ?, ? , ?, ?, ?)";
@@ -52,9 +56,10 @@ public class DAOEpreuveJdbcImpl implements DAOEpreuve {
 				ResultSet rs = rqt.getGeneratedKeys();
 				if (rs.next()) {
 					int id = rs.getInt(1);
-					data.setIdEpreuve(id);;
+					data.setIdEpreuve(id);
+					;
 				}
-		}
+			}
 		} catch (SQLException e) {
 			throw new DALException("Erreur DAL-add", e);
 		} finally {
@@ -68,7 +73,8 @@ public class DAOEpreuveJdbcImpl implements DAOEpreuve {
 		ResultSet rs = null;
 		Epreuve epreuve = null;
 		Test test = null;
-		
+		Candidat user = null;
+
 		try {
 			conn = ConnectionProvider.getCnx();
 			rqt = conn.prepareStatement(selectOne);
@@ -76,9 +82,19 @@ public class DAOEpreuveJdbcImpl implements DAOEpreuve {
 			rs = rqt.executeQuery();
 			while (rs.next()) {
 				test = new Test();
+				user = new Candidat();
 				epreuve = new Epreuve();
+
 				test.setIdTest(rs.getInt("t.idTest"));
-				
+				test.setLibelle(rs.getString("t.libelle"));
+				test.setDescription(rs.getString("t.description"));
+				test.setDuree(rs.getInt("t.duree"));
+
+				user.setIdUtilisateur(rs.getInt("u.idUtilisateur"));
+				user.setNom(rs.getString("u.nom"));
+				user.setPrenom(rs.getString("u.prenom"));
+				user.setEmail(rs.getString("u.email"));
+
 				epreuve.setIdEpreuve(rs.getInt("e.idEpreuve"));
 				epreuve.setDateDebutValidite(rs.getDate("e.dateDedutValidite"));
 				epreuve.setDateFinValidite(rs.getDate("e.dateFinValidite"));
@@ -86,25 +102,68 @@ public class DAOEpreuveJdbcImpl implements DAOEpreuve {
 				epreuve.setEtat(rs.getString("e.etat"));
 				epreuve.setNoteCandidat(rs.getFloat("e.note_obtenue"));
 				epreuve.setNiveauCandidat(rs.getString("e.niveau_obtenu"));
-				
+				epreuve.setTest(test);
+				epreuve.setCandidat(user);
+			}
+		} catch (SQLException e) {
+			throw new DALException("ERREUR DAL- select one " + e.getMessage() + e.getStackTrace().toString(), e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DALException("Erreur fermeture de connection", e);
 			}
 		}
-		return null;
+
+		return epreuve;
 	}
 
 	public List<Epreuve> selectAll() throws DALException {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	public void remove(int id) throws DALException {
-		// TODO Auto-generated method stub
+		PreparedStatement rqt = null;
+		try {
+			conn = ConnectionProvider.getCnx();
+			rqt = conn.prepareStatement(remove);
+			rqt.setInt(1, id);
+			rqt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DALException("ERREUR DAL - Remove", e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DALException("Erreur fermeture de connection", e);
+			}
+		}
 
 	}
 
 	public void update(Epreuve data) throws DALException {
-		// TODO Auto-generated method stub
-
+		PreparedStatement rqt = null;
+		try {
+			conn = ConnectionProvider.getCnx();
+			rqt = conn.prepareStatement(update);
+			rqt.setDate(1, data.getDateDebutValidite());
+			rqt.setDate(1, data.getDateFinValidite());
+			rqt.setInt(3, data.getTempsEcoule());
+			rqt.setString(4, data.getEtat());
+			rqt.setFloat(5, data.getNoteCandidat());
+			rqt.setString(6, data.getNiveauCandidat());
+			rqt.setInt(7, data.getTest().getIdTest());
+			rqt.setInt(8, data.getCandidat().getIdUtilisateur());
+			rqt.executeUpdate();
+		} catch (SQLException e) {
+			throw new DALException("Erreur DAL - updata", e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DALException("Erreur fermeture de connection", e);
+			}
+		}
 	}
-
 }
