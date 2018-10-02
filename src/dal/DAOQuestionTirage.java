62375aff87a7e0f8dab302b6ccfa9b51ee69828e
dlevallois2018 <dlevallois2018@27-UC37-23.ad.campus-eni.fr>
2018-10-02 15:15:26 +0200

    Revert "procedure + dal"
    This reverts commit 4f7c9fecd1527b1d581efc811ed61e7ce9085e4a.

--- src/dal/DAOQuestionTirage.java
+++ src/dal/DAOQuestionTirage.java
@@ -1,10 +1,7 @@
 package dal;
 
-import java.util.List;
-
 import bo.QuestionTirage;
 
 public interface DAOQuestionTirage extends DAOGeneric<QuestionTirage> {
-	public void generationTest(int idEpreuve) throws DALException;
-	public List<QuestionTirage> getQuestionTirageDansLOrdre(int idEpreuve) throws DALException;
+
 }
