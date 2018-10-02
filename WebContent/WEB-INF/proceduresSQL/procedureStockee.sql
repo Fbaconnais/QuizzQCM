b4f8c1cbcfe7f9d243191949980995917f0f20be
dlevallois2018 <dlevallois2018@27-UC37-23.ad.campus-eni.fr>
2018-10-02 15:36:12 +0200

    hgefthrttrhrt

--- WebContent/WEB-INF/proceduresSQL/procedureStockee.sql
+++ WebContent/WEB-INF/proceduresSQL/procedureStockee.sql
@@ -10,7 +10,7 @@ BEGIN
 	@idQuestion int
 
 	DECLARE
-	questions CURSOR FOR SELECT TOP (@nbQuestions) idQuestion,ROW_NUMBER() over(ORDER BY NEWID()) as ordre FROM QUESTION WHERE idTheme = @idtheme
+	questions CURSOR FOR SELECT TOP (@nbQuestions) idQuestion,ROW_NUMBER() over(ORDER BY NEWID()) as ordre FROM QUESTION WHERE idTheme = @idtheme 
 	 
 	DECLARE
 	@ordre int
@@ -20,9 +20,10 @@ BEGIN
 		WHILE @@FETCH_STATUS = 0
 			BEGIN
 				INSERT INTO QUESTION_TIRAGE (estMarquee,idQuestion,numordre,idEpreuve) VALUES ('false',@idQuestion,@ordre,@idEpreuve)
-				FETCH NEXT FROM questions INTO @idQuestion
+				FETCH NEXT FROM questions INTO @idQuestion,@ordre
 			END
 	CLOSE questions 
 	DEALLOCATE questions
 END
 GO
+
