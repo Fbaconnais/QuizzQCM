ad80d108e26015132252f92727b09c8725664881
fbaconnais2018 <fbaconnais2018@27-UC37-24.ad.campus-eni.fr>
2018-10-03 11:40:23 +0200

    Update QuestionManager

---
+++ src/bll/QuestionManager.java
@@ -0,0 +1,42 @@
+package bll;
+
+import java.util.List;
+
+import bo.Proposition;
+import bo.Question;
+import dal.DALException;
+import dal.DAOFactory;
+import dal.DAOQuestion;
+
+public class QuestionManager {
+	private DAOQuestion DAOQuestion;
+	
+	private QuestionManager() {
+		DAOQuestion = DAOFactory.getDAOQuestion();
+	}
+	
+	public static QuestionManager getMger() {
+		return new QuestionManager();
+	}
+	
+	public Question selectQuestion(int id) throws BLLException {
+		Question question = null;
+		try {
+			question = DAOQuestion.selectOne(id);
+		} catch (DALException e) {
+			throw new BLLException(e.getMessage(), e);
+		}
+		
+		return question;
+	}
+	
+	public List<Proposition> getPropositionsByIDQuestion(int id) throws BLLException{
+		List<Proposition> listePropositions = null;
+		try {
+			listePropositions = DAOQuestion.selectAllByIDQuestion(id);
+		} catch (DALException e) {
+			throw new BLLException(e.getMessage(), e);
+		}
+		return listePropositions;
+	}
+}
