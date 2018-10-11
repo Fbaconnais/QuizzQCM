8e9a4e898fe99f05e52c596d58ccf35377f28c5b
dlevallois2018 <dlevallois2018@27-UC37-23.ad.campus-eni.fr>
2018-10-11 09:47:50 +0200

    restzergfze

---
+++ test/bo/CandidatTest.java
@@ -0,0 +1,106 @@
+package bo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CandidatTest {
+
+	
+
+	@Test
+	public void testGetPromotion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPromotion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCandidatPromotion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetIdUtilisateur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetIdUtilisateur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrenom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPrenom() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPassword() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUtilisateur() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUtilisateurStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUtilisateurIntStringStringStringString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProfil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetProfil() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUtilisateurIntStringStringStringStringProfil() {
+		fail("Not yet implemented");
+	}
+
+}
