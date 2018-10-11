aa229474561aa51395d42b8e9f5aef5546e142fa
fbaconnais2018 <fbaconnais2018@27-UC37-24.ad.campus-eni.fr>
2018-10-11 15:04:45 +0200

    DAOTP6H5fzk i d a o pr

--- src/dal/jdbc/DAOPropositionJdbcImpl.java
+++ src/dal/jdbc/DAOPropositionJdbcImpl.java
@@ -1,16 +1,43 @@
 package dal.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 import bo.Proposition;
+import dal.ConnectionProvider;
 import dal.DALException;
 import dal.DAOProposition;
 
 public class DAOPropositionJdbcImpl implements DAOProposition {
+	String add = "INSERT INTO Proposition (enonce, idQuestion, estBonne) VALUES (?, ?, ?)";
+	
 
 	@Override
 	public Proposition add(Proposition data) throws DALException {
-		// TODO Auto-generated method stub
+		PreparedStatement rqt = null;
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getCnx();
+			rqt = conn.prepareStatement(add, Statement.RETURN_GENERATED_KEYS);
+			rqt.setString(1, data.getEnonce());
+			rqt.setInt(2, data.getIdQuestion());;
+			rqt.setBoolean(3, data.getEstBonne());;
+			rqt.executeUpdate();
+		
+		} catch (SQLException e) {
+			throw new DALException("Erreur DAL-add", e);
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return null;
 	}
 
